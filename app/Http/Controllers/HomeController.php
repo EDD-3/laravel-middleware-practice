64af3d7f25c1148209b6554c0db304b6cd53b8ec
Ed <eduramq@gmail.com>
2023-01-10 23:20:33 -0700

    Flashing data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,13 +26,32 @@ class HomeController extends Controller
         //Setting session globally
         //session(['peter'=>'student']);
         //Setting session locally
-        $request->session()->put(['edwin'=>'master instructor']);
+        // $request->session()->put(['edwin'=>'master instructor']);
         
-        //Access session
-        return $request->session()->all();
+        //Deleting session
+        // $request->session()->forget('edwin2');
 
+        //Deleting every session
+        // $request->session()->flush();
+
+        //Access session
+        // return $request->session()->all();
+
+        // session(['edwin2'=>'your teacher']);
+
+        // return session('edwin2');
+        
 
 
         // return view('home');
+
+        //Flashing session
+        // $request->session()->flash('message','Post has been created');
+        // return $request->session()->get('message');
+
+        //Keep flash data
+        $request->session->reflash();
+
+        $request->session()->keep('message');
     }
 }
